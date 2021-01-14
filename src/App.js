d21a5d2c8c3a0e9456fcc323505b1897f9b6a6a9
Marcelin <marsbob@live.fr>
2021-01-14 18:07:52 +0100

    exo 6 terminé

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Article from './modules/Article'
+import Body from './modules/Body'
+import Footer from './modules/Footer'
+import Header from './modules/Header'
+import Nav from './modules/Nav'
+import SearchBar from './modules/SearchBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Body></Body>
     </div>
   );
 }
